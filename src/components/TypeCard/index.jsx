cbf802a9a9faca8995af07b06635c49a30da8a75
pearbug <ufr134@naver.com>
2024-06-12 00:23:10 +0900

    Poke Type Calc v2.0 (#7)
    * class 구조변경
    
    * 이미지 및 css수정
    
    * v2
    
    * v2-오타 및 가독성 수정
    
    * v2-오타 및 가독성 수정
    
    * fix: CSS type-button-checked color
    
    * fix: manifest.json
    
    * delete: dist directory
    
    * update: .gitignore
    
    * v2.0 테스트 및 Update Info 작성
    
    * refactor : package.json 및 manifest.json 에 v2.0 기입
    
    * refactor : manifest.json 내의 불필요한 권한 요청 제거
    
    ---------
    
    Co-authored-by: heisje <heeje1996@gmail.com>

---
+++ src/components/TypeCard/index.jsx
@@ -0,0 +1,12 @@
+import {usTypes} from "../../data/types.js";
+
+const TypeCard = ({text, index, ...rest}) => {
+    return <button {...rest}>
+        <img src={`/types/${usTypes[index]}.png`} alt="Type Image"/>
+        <div className="flex">
+            <label>{text}</label>
+        </div>
+    </button>
+}
+
+export default TypeCard;
