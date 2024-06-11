ee42394cc05f078ab01e4f0426c47dac7b86a680
heisje <heeje1996@gmail.com>
2024-06-12 01:54:32 +0900

    fix: typecard img root

--- src/components/TypeCard/index.jsx
+++ src/components/TypeCard/index.jsx
@@ -2,7 +2,7 @@ import {usTypes} from "../../data/types.js";
 
 const TypeCard = ({text, index, ...rest}) => {
     return <button {...rest}>
-        <img src={`/types/${usTypes[index]}.png`} alt="Type Image"/>
+        <img src={`types/${usTypes[index]}.png`} alt="Type Image"/>
         <div className="flex">
             <label>{text}</label>
         </div>
