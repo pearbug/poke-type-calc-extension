ccdce339fa705fb00c72638c5f4cfbc94b70071f
heisje <heeje1996@gmail.com>
2024-06-13 17:53:22 +0900

    fix: Header UI smaller

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,18 @@ function App() {
         <>
             <BrowserRouter>
                 <StyledNav>
-                    <NavLink to="/" className={({isActive}) => (isActive ? 'active' : '')}>
-                        상성계산기
-                    </NavLink>
-                    <NavLink to="/nature" className={({isActive}) => (isActive ? 'active' : '')}>
-                        성격표
-                    </NavLink>
-                    <NavLink to="/base-stats" className={({isActive}) => (isActive ? 'active' : '')}>
-                        종족값검색
-                    </NavLink>
+                    <img src={`logo.png`} alt="logo"/>
+                    <div>
+                        <NavLink to="/" className={({isActive}) => (isActive ? 'active' : '')}>
+                            상성계산기
+                        </NavLink>
+                        <NavLink to="/nature" className={({isActive}) => (isActive ? 'active' : '')}>
+                            성격표
+                        </NavLink>
+                        <NavLink to="/base-stats" className={({isActive}) => (isActive ? 'active' : '')}>
+                            종족값검색
+                        </NavLink>
+                    </div>
                 </StyledNav>
 
                 <Routes>
